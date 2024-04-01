ef4ecdff674c7abb3a9c3001a5faa8804d954c96
call-me-suman <sumansuriya7010@gmail.com>
2024-04-01 20:03:27 +0530

    hgdujaweg

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,23 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          port: ''
-        }
-      ]
-    }
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: ''
+      }
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
